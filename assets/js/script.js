cc1b480d1cde3b356728dd98443cf78dacde2c09
NeXFP <giovannycardoso50@gmail.com>
2021-09-17 04:30:42 -0400

    Added and relocated things to different files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,29 @@
+// For Buttons
+const highScore = document.getElementById('highScore');
+const start = document.getElementById('start');
+const home = document.getElementById('home');
+
+// For Sections
+const intro = document.getElementById('intro');
+const quiz = document.getElementById('quiz');
+const hsPage = document.getElementById('hsPage');
+
+// Some References
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
+const scoreEl = document.querySelector('#score');
+const timerEl = document.getElementById('timer');
 
+// Some Variables
 let currentQuestion = {};
 let acceptingAnswers = true;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
+const correct_bonus = 10;
+const max_questions = 4;
 
+// Question and Answer List
 let questions = [
     {
         question: "What is the correct syntax for referring to an external script called?",
@@ -42,19 +59,34 @@ let questions = [
     } 
 ];
 
-const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 4;
+// Home Page Screen
+function homePage() {
+    intro.style.display = "flex"
+    quiz.style.display = "none";
+    hsPage.style.display = "none";
+}
 
-startGame = () => {
+// Order of the Quiz
+function startQuiz() {
     questionCounter = 0;
-    score = 0;
+    intro.style.display = "none";
     availableQuestions = [... questions];
+    timer();
+    quiz.style.display = "flex";
     getNewQuestion(); 
 };
 
+
+// Highscore Screen
+function highscoreScreen() {
+    intro.style.display = "none";
+    hsPage.style.display = "flex";
+}
+
+// Function to get a new question
 getNewQuestion = () => {
     questionCounter++;
-    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    const questionIndex = questionInOrder();
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
 
@@ -63,20 +95,61 @@ getNewQuestion = () => {
         choice.innerText = currentQuestion['choice' + number];
     });
     availableQuestions.splice(questionIndex, 1);
-
     acceptingAnswers = true;
 };
 
+// Number generater for new question function
+var questionInOrder = function() {
+    for (i = 0; i < 4; i++) {
+        if (i > 3) {
+            highscorePage();
+        } else {
+            return i;
+        }
+    }
+}
+
+// Function for the timer
+function timer() {
+    var timeLeft = 60;
+
+    var timeInterval = setInterval(function() {
+        if (timeLeft >= 0) { 
+            timerEl.innerText = timeLeft;
+            timeLeft--;
+        } else {
+            clearInterval(timeInterval);
+        }
+    }, 1000);
+}
+
+// Function for the score
+function setCounterText() {
+    scoreEl.textContent = count;
+}
+
+// Additional function for the score
 choices.forEach(choice => {
     choice.addEventListener("click", e => {
-       // if(acceptingAnswers) return;
+       //if(acceptingAnswers) return;
 
         acceptingAnswers = false;
         const selectedChoice = e.target;
         const selectedAnswer = selectedChoice.dataset["number"];
 
+        /*
+        if((selectedAnswer === 1 && answer === 1) || (selectedAnswer === 2 && answer === 2) || (selectedAnswer === 3 && answer === 3) || (selectedAnswer === 4 && answer === 4)) {
+            score++;
+            setCounterText();
+        }
+        */
+
         getNewQuestion();
     });
 });
 
-startGame();
+// Event Listeners for buttons
+start.addEventListener("click", startQuiz)
+highScore.addEventListener("click", highscoreScreen)
+home.addEventListener("click", homePage)
+
